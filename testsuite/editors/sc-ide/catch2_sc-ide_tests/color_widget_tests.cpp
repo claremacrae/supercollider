70059badb8c6c55dab31ac1a60312ad85b394c50
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-01 21:11:32 +0000

    testsuite: Add Catch StringMaker for QColor

--- testsuite/editors/sc-ide/catch2_sc-ide_tests/color_widget_tests.cpp
+++ testsuite/editors/sc-ide/catch2_sc-ide_tests/color_widget_tests.cpp
@@ -3,21 +3,45 @@
 // TODO Could move this test to a sub-directory widgets/util, as number of tests grows
 #include "widgets/util/color_widget.hpp"
 
+#include <QStringBuilder>
+#include <QStringLiteral>
+#include <iostream>
+
 using namespace ScIDE;
+
+// https://github.com/catchorg/Catch2/blob/master/docs/tostring.md#top
+namespace Catch {
+    template<>
+    struct StringMaker<QColor> {
+        static std::string convert( QColor const& color ) {
+            const QString result =
+                    "(" %
+                    QString::number(color.redF()) % ", " %
+                    QString::number(color.greenF()) % ", " %
+                    QString::number(color.blueF()) %
+                    "), alpha = " %
+                    QString::number(color.alphaF(), 'g', 20);
+            return result.toStdString();
+        }
+    };
+}
 
 // TODO Consider adding tags for tests
 TEST_CASE("ColorWidget initial state") {
     ColorWidget widget;
-    // TODO Introduce a way to pretty-print QColor for nicer test output, to save having to call .name()
-    QColor expected_color;
-    CHECK(widget.color().name().toStdString() == expected_color.name().toStdString());
-    CHECK(widget.color().alpha() == expected_color.alpha());
+    QColor expected_color(0, 0, 0, 255);
+    CHECK(widget.color() == expected_color);
 }
 
 TEST_CASE("ColorWidget changing color updates and emits correctly") {
+    // Arrange
     ColorWidget widget;
     QColor red("red");
-    CHECK(widget.color().name().toStdString() == red.name().toStdString());
-    CHECK(widget.color().alpha() == red.alpha());
+
+    // Act
+    widget.setColor(red);
+
+    // Assert
+    CHECK(widget.color() == red);
     // TODO Check that signals are emitted correctly, and only once
 }
