24686841a360cd604fb4eec1ff8918ac83b2e1ec
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-01 20:24:29 +0000

    testsuite: Add some Catch2 tests for ColorWidget

--- testsuite/editors/sc-ide/catch2_sc-ide_tests/color_widget_tests.cpp
+++ testsuite/editors/sc-ide/catch2_sc-ide_tests/color_widget_tests.cpp
@@ -5,8 +5,19 @@
 
 using namespace ScIDE;
 
-TEST_CASE("ColorWidget shows without crashing") {
+// TODO Consider adding tags for tests
+TEST_CASE("ColorWidget initial state") {
     ColorWidget widget;
-    widget.show();
-    widget.raise();
+    // TODO Introduce a way to pretty-print QColor for nicer test output, to save having to call .name()
+    QColor expected_color;
+    CHECK(widget.color().name().toStdString() == expected_color.name().toStdString());
+    CHECK(widget.color().alpha() == expected_color.alpha());
+}
+
+TEST_CASE("ColorWidget changing color updates and emits correctly") {
+    ColorWidget widget;
+    QColor red("red");
+    CHECK(widget.color().name().toStdString() == red.name().toStdString());
+    CHECK(widget.color().alpha() == red.alpha());
+    // TODO Check that signals are emitted correctly, and only once
 }
