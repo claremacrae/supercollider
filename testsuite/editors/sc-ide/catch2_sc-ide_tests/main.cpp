fd09dc919c9dda3494728ed17a651dd035a4982c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-02 17:47:57 +0000

    testsuite: Fix code formatting

--- testsuite/editors/sc-ide/catch2_sc-ide_tests/main.cpp
+++ testsuite/editors/sc-ide/catch2_sc-ide_tests/main.cpp
@@ -4,11 +4,11 @@
 #include <QApplication>
 
 // For explanation of Catch code, see https://github.com/catchorg/Catch2/blob/master/docs/own-main.md
-int main( int argc, char* argv[] ) {
+int main(int argc, char* argv[]) {
     Catch::Session session;
 
-    int returnCode = session.applyCommandLine( argc, argv );
-    if( returnCode != 0 ) // Indicates a command line error
+    int returnCode = session.applyCommandLine(argc, argv);
+    if (returnCode != 0) // Indicates a command line error
         return returnCode;
 
     // Set up exactly one QApplication, so we can create QWidgets in tests
--- testsuite/editors/sc-ide/catch2_sc-ide_tests/test_helpers/catch_qt_string_makers.hpp
+++ testsuite/editors/sc-ide/catch2_sc-ide_tests/test_helpers/catch_qt_string_makers.hpp
@@ -7,16 +7,10 @@
 
 // https://github.com/catchorg/Catch2/blob/master/docs/tostring.md#top
 namespace Catch {
-template<>
-struct StringMaker<QColor> {
-    static std::string convert( QColor const& color ) {
-        const QString result =
-                "(" %
-                QString::number(color.redF()) % ", " %
-                QString::number(color.greenF()) % ", " %
-                QString::number(color.blueF()) %
-                "), alpha = " %
-                QString::number(color.alphaF(), 'g', 20);
+template <> struct StringMaker<QColor> {
+    static std::string convert(QColor const& color) {
+        const QString result = "(" % QString::number(color.redF()) % ", " % QString::number(color.greenF()) % ", "
+            % QString::number(color.blueF()) % "), alpha = " % QString::number(color.alphaF(), 'g', 20);
         return result.toStdString();
     }
 };
