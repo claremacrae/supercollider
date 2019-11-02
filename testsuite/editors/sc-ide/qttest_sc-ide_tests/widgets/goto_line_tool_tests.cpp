d9e32385584edde54d8d152bc2cb2154c4434799
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-02 21:56:17 +0000

    testsuite: Add a real test for GoToLineTool's signal

--- testsuite/editors/sc-ide/qttest_sc-ide_tests/widgets/goto_line_tool_tests.cpp
+++ testsuite/editors/sc-ide/qttest_sc-ide_tests/widgets/goto_line_tool_tests.cpp
@@ -1,4 +1,5 @@
 #include <QTest>
+#include <QSignalSpy>
 
 #include "goto_line_tool.hpp"
 
@@ -7,10 +8,51 @@ using namespace ScIDE;
 class GoToLineToolTests : public QObject {
 Q_OBJECT
 private slots:
-    void GoToLineConstructorDoesNotCrash() {
+    void testSignalEmittedWhenGoButtonClicked() {
         GoToLineTool widget;
-        // TODO Add a meaningful test
+        widget.raise();
+        widget.show();
+        widget.setMaximum(27);
+
+        auto spinner = widget.findChild<QSpinBox*>();
+        QVERIFY2(spinner, "This test is no longer valid: the class being test has been changed");
+
+        auto goButton = widget.findChild<QToolButton*>();
+        QVERIFY2(goButton, "This test is no longer valid: the class being test has been changed");
+
+        widget.setFocus();
+        QApplication::processEvents(QEventLoop::AllEvents);
+        QVERIFY(spinner->hasFocus());
+
+        QSignalSpy activatedSpy(&widget, SIGNAL(activated(int)));
+        QVERIFY(activatedSpy.isValid());
+
+        // Type a number
+        QTest::keyClicks(spinner, "1");
+        QTest::keyClicks(spinner, "7");
+        QApplication::processEvents(QEventLoop::AllEvents);
+
+        // Check that no signals have yet been emitted:
+        QCOMPARE(activatedSpy.count(), 0);
+
+        // Clicking the Go button:
+        goButton->click();
+        QApplication::processEvents(QEventLoop::AllEvents);
+        QVERIFY(spinner->hasFocus());
+
+        activatedSpy.wait(1000);
+        QVERIFY(spinner->hasFocus());
+
+        // Check the activated() signal was emitted only once:
+        QCOMPARE(activatedSpy.count(), 1);
+
+        // And check that the signal emitted the correct value
+        QList<QVariant> arguments = activatedSpy.takeFirst();
+        QVariant argument = arguments.at(0);
+        QVERIFY(argument.type() == QVariant::Int);
+        QVERIFY(argument.toInt() == 17);
     }
+
 };
 
 QTEST_MAIN(GoToLineToolTests)
