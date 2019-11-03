14e3bd633199a1c9c8ef19ca63910e8730229a0f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-03 20:57:50 +0000

    testsuite: Turn off clang-format in one function for readability

--- testsuite/editors/sc-ide/boosttest_sc-ide_tests/test_helpers/boosttest_qt_string_makers.hpp
+++ testsuite/editors/sc-ide/boosttest_sc-ide_tests/test_helpers/boosttest_qt_string_makers.hpp
@@ -3,11 +3,13 @@
 #include <QColor>
 
 std::ostream& boost_test_print_type(std::ostream& ostr, QColor const& color) {
+    // clang-format off
     ostr << "(" <<
         color.redF() << ", " <<
         color.greenF() << ", " <<
         color.blueF() <<
         "), alpha = " <<
         color.alphaF();
+    // clang-format on
     return ostr;
 }
