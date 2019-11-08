e8618d614708d28e6176dddb13915736ebe4ea49
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-08 10:23:53 +0000

    testsuite: Move some setup code to new init() slot

--- testsuite/editors/sc-ide/qttest_sc-ide_tests/widgets/goto_line_tool_tests.cpp
+++ testsuite/editors/sc-ide/qttest_sc-ide_tests/widgets/goto_line_tool_tests.cpp
@@ -9,28 +9,32 @@ using namespace ScIDE;
 
 class GoToLineToolTests : public QObject {
     Q_OBJECT
+private:
+    std::auto_ptr<GoToLineTool> widget;
 private slots:
-    void testSignalEmittedWhenGoButtonClicked() {
-        GoToLineTool widget;
-        widget.raise();
-        widget.show();
+    void init() {
+        widget = std::auto_ptr<GoToLineTool>(new GoToLineTool);
+        widget->raise();
+        widget->show();
         // TODO Maybe use QTest::qWaitForWindowExposed()
         // https://doc.qt.io/qt-5/qtest.html#qWaitForWindowExposed
         // TODO Do we need to create a QWindow first, as parent for this?
         // TODO Do we need to make sure widget has a non-null parent?
-        widget.setMaximum(27);
-
-        auto spinner = widget.findChild<QSpinBox*>();
-        QVERIFY2(spinner, "This test is no longer valid: the class being test has been changed");
-
-        auto goButton = widget.findChild<QToolButton*>();
-        QVERIFY2(goButton, "This test is no longer valid: the class being test has been changed");
 
         // This is needed to may sure that text in the spinner is selected, so that
         // as we type in characters, the initial text ("1") is erased
-        widget.setFocus();
+        widget->setFocus();
+    }
+    void testSignalEmittedWhenGoButtonClicked() {
+        widget->setMaximum(27);
 
-        QSignalSpy activatedSpy(&widget, SIGNAL(activated(int)));
+        auto spinner = widget->findChild<QSpinBox*>();
+        QVERIFY2(spinner, "This test is no longer valid: the class being test has been changed");
+
+        auto goButton = widget->findChild<QToolButton*>();
+        QVERIFY2(goButton, "This test is no longer valid: the class being test has been changed");
+
+        QSignalSpy activatedSpy(widget.get(), SIGNAL(activated(int)));
         QVERIFY(activatedSpy.isValid());
 
         // Type a number, one digit at a time
