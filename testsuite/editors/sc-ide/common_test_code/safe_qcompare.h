37f29813072961fd635453190907f29358d8d63c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-09-15 11:48:19 +0100

    Add comments - and credit Fabian Kosmale at Qt for the code

--- testsuite/editors/sc-ide/common_test_code/safe_qcompare.h
+++ testsuite/editors/sc-ide/common_test_code/safe_qcompare.h
@@ -11,6 +11,14 @@ namespace qft {
 void sender(...);
 }
 
+// A custom QCOMPARE implementation, for use with tests that are driven
+// by Catch2, or any other non-QtTest main(), that want access to QtTest
+// in order to use QSignalSpy - but must not use QCOMPARE and similar,
+// as any failures in these macros are silently ignored when called
+// from outside a QObject slot.
+//
+// Credit: Fabian Kosmale at Qt, November 2019
+
 #undef QCOMPARE
 #define QCOMPARE(actual, expected) \
 do {using namespace qft;\
