1884404dbb53d36eaaa642862389adc87f5c85f3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-09-15 10:31:59 +0100

    Add safer QCOMPARE implementation from Fabian Kosmale at Qt
    This static_asserts that QCOMPARE is used inside a QOBJECT slot

---
+++ testsuite/editors/sc-ide/common_test_code/safe_qcompare.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <type_traits>
+#include <QCoreApplication>
+#include <QTest>
+
+namespace qft {
+
+void sender(...);
+}
+
+#undef QCOMPARE
+#define QCOMPARE(actual, expected) \
+do {using namespace qft;\
+    static_assert(std::is_same_v<decltype(sender()), QObject*>, "Cannot user QCOMPARE outside of a QObject slot");\
+    if (!QTest::qCompare(actual, expected, #actual, #expected, __FILE__, __LINE__))\
+        return;\
+} while (false)
